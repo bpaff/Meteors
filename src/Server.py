6aa1ec523b7b424d091c0154e9d2fa76996f11e7
Chelsea Schneider <clschneider@yahoo.com>
2014-04-28 18:24:21 -0700

    Added Network/Server -- Code from last homework

---
+++ src/Server.py
@@ -0,0 +1,34 @@
+from Network import Listener, Handler, poll
+import sys
+import Network
+
+handlers = []
+ 
+class MyHandler(Handler):
+     
+    def on_open(self):
+        self.userName = "John Doe"
+        handlers.append(self)
+         
+    def on_close(self):
+        self.on_msg({'leave': self.userName})
+        handlers.remove(self)
+     
+    def on_msg(self, msg):
+        if msg.has_key('join'):
+            self.userName = msg['join']
+        for h in handlers:
+            if h != self:
+                h.do_send(msg)
+
+
+class Server:
+    def run(self):
+        port = 8888
+        s = Listener(port, MyHandler)
+        while 1:
+            poll(timeout=0.05) # in seconds
+
+# Uncomment to run the server            
+#server = Server()
+#server.loop()       
